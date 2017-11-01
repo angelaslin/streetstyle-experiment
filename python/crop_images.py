5728cb7f53c6e68bdd44338da83181fc4262e14c
Angela Lin <angela.shih.lin@gmail.com>
2017-11-01 12:13:08 -0500

    Changed crop images to include the id of the entry. There may be multiple bounding boxes for the same Instagram post.

--- python/crop_images.py
+++ python/crop_images.py
@@ -4,11 +4,11 @@ import sqlite3
 from PIL import Image
 import os.path
 
-def crop_image(image_path, x1, y1, x2, y2):
+def crop_image(image_id,image_path, x1, y1, x2, y2):
     im = Image.open(image_path)
     cropped = im.crop((x1, y1, x2, y2))
     root, ext = os.path.splitext(image_path)
-    new_image_path = root + "_cropped" + ext
+    new_image_path = root + "_cropped_" + str(image_id) + ext
     cropped.save(new_image_path)
     im.close()
     cropped.close()
@@ -19,10 +19,10 @@ def crop_all_images():
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
     # loop over all fields
-    entries = cursor.execute("SELECT url, x1, y1, x2, y2 FROM streetstyle27k")
+    entries = cursor.execute("SELECT id, url, x1, y1, x2, y2 FROM streetstyle27k")
     for e in entries:
-        if e[0] != '' and e[1] != '' and e[2] != '' and e[3] != '' and e[4] != '':
-            crop_image(e[0], int(e[1]), int(e[2]), int(e[3]), int(e[4]))
+        if e[0] != '' and e[1] != '' and e[2] != '' and e[3] != '' and e[4] != '' and e[5] != '':
+            crop_image(int(e[0]), e[1], int(e[2]), int(e[3]), int(e[4]), int(e[5]))
     print('Done')
     conn.close()
 
--- python/examples.py
+++ python/examples.py
@@ -30,7 +30,7 @@ def get_examples():
     # open sqlite3 database
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    query = "SELECT url FROM streetstyle27k WHERE {}={} LIMIT 10"
+    query = "SELECT id, url FROM streetstyle27k WHERE {}={} LIMIT 10"
     for attribute, values in attributes_dict.iteritems():
         attribute_path = os.path.join(examples_path, attribute)
         try:
@@ -45,10 +45,11 @@ def get_examples():
                 pass
             entries = cursor.execute(query.format(attribute, value))   
             for entry in entries:
-                image_path = entry[0]
-                if image_path != '':
+                image_path = entry[1]
+                image_id = entry[0]
+                if image_id != '' and image_path != '':
                     root, ext = os.path.splitext(image_path)
-                    cropped_image_path = root + '_cropped' + ext
+                    cropped_image_path = root + '_cropped_' + str(image_id) + ext
                     copyfile(image_path, os.path.join(value_path, os.path.basename(image_path)))
                     copyfile(cropped_image_path, os.path.join(value_path, os.path.basename(cropped_image_path)))
     print("Done processing examples")
